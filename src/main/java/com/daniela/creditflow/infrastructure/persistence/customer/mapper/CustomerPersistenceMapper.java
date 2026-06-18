f580c60a5061b68c94997d1f55f507bde843c44b
Danniesantos <danielardesantos@gmail.com>
2026-06-18 19:41:31 -0300

    feat(customer): add customer persistence mapper

---
+++ src/main/java/com/daniela/creditflow/infrastructure/persistence/customer/mapper/CustomerPersistenceMapper.java
@@ -0,0 +1,52 @@
+package com.daniela.creditflow.infrastructure.persistence.customer.mapper;
+
+import com.daniela.creditflow.domain.customer.model.*;
+import com.daniela.creditflow.domain.customer.valueObject.CPF;
+import com.daniela.creditflow.domain.customer.valueObject.CustomerId;
+import com.daniela.creditflow.domain.customer.valueObject.Email;
+import com.daniela.creditflow.domain.customer.valueObject.PhoneNumber;
+import com.daniela.creditflow.domain.valueObject.*;
+import com.daniela.creditflow.infrastructure.persistence.customer.entity.CustomerEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerPersistenceMapper {
+
+    public CustomerEntity toEntity(Customer customer) {
+
+        return new CustomerEntity(
+                customer.getId().value(),
+                customer.getName(),
+                customer.getCpf().value(),
+                customer.getEmail().value(),
+                customer.getDateOfBirth(),
+                customer.getPhoneNumber().value(),
+                customer.getMonthlyIncome().value(),
+                customer.getCreditScore().value(),
+                customer.getStatus(),
+                customer.getCreatedAt(),
+                customer.getUpdatedAt()
+        );
+    }
+
+    public Customer toDomain(CustomerEntity entity) {
+        CustomerData data = new CustomerData(
+                entity.getName(),
+                new CPF(entity.getCpf()),
+                new Email(entity.getEmail()),
+                entity.getDateOfBirth(),
+                new PhoneNumber(entity.getPhoneNumber()),
+                new CreditScore(entity.getCreditScore()),
+                new Money(entity.getMonthlyIncome())
+        );
+
+        return new Customer(
+                new CustomerId(entity.getId()),
+                data,
+                entity.getStatus(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt()
+        );
+
+    }
+}
