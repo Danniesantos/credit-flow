3435cfbc803bd814e3254cb0405e74ec04c0393b
Danniesantos <danielardesantos@gmail.com>
2026-06-19 15:47:43 -0300

    initial commit

---
+++ src/main/java/com/daniela/creditflow/CreditFlowApplication.java
@@ -0,0 +1,13 @@
+package com.daniela.creditflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditFlowApplication.class, args);
+	}
+
+}
