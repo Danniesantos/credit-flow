4811a2c40b968c94b577369e7adef43c787a1b91
Danniesantos <danielardesantos@gmail.com>
2026-06-19 16:00:13 -0300

    initial commit

---
+++ src/test/java/com/daniela/creditflow/CreditFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniela.creditflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
