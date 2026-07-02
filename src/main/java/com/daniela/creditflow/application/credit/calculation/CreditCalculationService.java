86ed9b4ff73319e6ee21ff035fd77edb61e58f27
Danniesantos <danielardesantos@gmail.com>
2026-07-02 14:42:35 -0300

    feat(credit): implement credit calculation service

---
+++ src/main/java/com/daniela/creditflow/application/credit/calculation/CreditCalculationService.java
@@ -0,0 +1,30 @@
+package com.daniela.creditflow.application.credit.calculation;
+
+import com.daniela.creditflow.domain.credit.model.CreditType;
+import com.daniela.creditflow.domain.valueObject.Money;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreditCalculationService {
+
+    private final CreditStrategyFactory strategyFactory;
+
+    public CreditCalculationService(
+            CreditStrategyFactory strategyFactory) {
+
+        this.strategyFactory = strategyFactory;
+    }
+
+    public CreditCalculationResult calculate(
+            CreditType creditType,
+            Money requestedAmount,
+            Integer installments) {
+
+        return strategyFactory
+                .create(creditType)
+                .calculate(
+                        requestedAmount,
+                        installments);
+    }
+}
+
