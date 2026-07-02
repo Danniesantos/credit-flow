8d1dbbbf1ec7ee044293912f965f66f1a8db6bc1
Danniesantos <danielardesantos@gmail.com>
2026-07-02 11:12:41 -0300

    feat(domain): implement domain exception

--- src/main/java/com/daniela/creditflow/domain/exceptions/DomainException.java
+++ src/main/java/com/daniela/creditflow/domain/exceptions/DomainException.java
@@ -1,6 +1,6 @@
 package com.daniela.creditflow.domain.exceptions;
 
-public abstract class DomainException extends RuntimeException {
+public class DomainException extends RuntimeException {
     public DomainException(String message) {
         super(message);
     }
