00abacef2ef498a464bc219ccab566a9f88eef60
Danniesantos <danielardesantos@gmail.com>
2026-07-02 15:39:17 -0300

    feat(credit): implement find credit use case

---
+++ src/main/java/com/daniela/creditflow/application/credit/usecase/FindCreditUseCase.java
@@ -0,0 +1,31 @@
+package com.daniela.creditflow.application.credit.usecase;
+
+import com.daniela.creditflow.application.credit.dto.output.CreditDetailsOutput;
+import com.daniela.creditflow.application.credit.mapper.CreditOutputMapper;
+import com.daniela.creditflow.domain.credit.model.Credit;
+import com.daniela.creditflow.domain.credit.repository.CreditRepository;
+import com.daniela.creditflow.domain.credit.valueObject.CreditId;
+import com.daniela.creditflow.domain.exceptions.CreditNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FindCreditUseCase {
+
+    private final CreditRepository repository;
+    private final CreditOutputMapper creditMapper;
+
+    public FindCreditUseCase(CreditRepository repository,
+                             CreditOutputMapper creditMapper) {
+
+        this.repository = repository;
+        this.creditMapper = creditMapper;
+    }
+
+    public CreditDetailsOutput execute(CreditId creditId) {
+        Credit credit = repository
+                .findById(creditId)
+                .orElseThrow(() -> new CreditNotFoundException(creditId));
+
+        return creditMapper.toDetailsOutput(credit);
+    }
+}
