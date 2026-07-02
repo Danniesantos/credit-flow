56fbc5320a6787062f35a95927365dbf2bde11fb
Danniesantos <danielardesantos@gmail.com>
2026-07-02 11:30:45 -0300

    feat(credit): define credit repository interface

--- src/main/java/com/daniela/creditflow/domain/credit/repository/CreditRepository.java
+++ src/main/java/com/daniela/creditflow/domain/credit/repository/CreditRepository.java
@@ -1,7 +1,7 @@
 package com.daniela.creditflow.domain.credit.repository;
 
 import com.daniela.creditflow.domain.credit.model.Credit;
-import com.daniela.creditflow.domain.valueObject.CreditId;
+import com.daniela.creditflow.domain.credit.valueObject.CreditId;
 import com.daniela.creditflow.domain.customer.valueObject.CustomerId;
 
 import java.util.Optional;
