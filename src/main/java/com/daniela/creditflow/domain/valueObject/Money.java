88f95254fdbc297d5b760134c078b2184d2b6753
Danniesantos <danielardesantos@gmail.com>
2026-07-02 11:39:18 -0300

    feat(domain): implement money value object

---
+++ src/main/java/com/daniela/creditflow/domain/valueObject/Money.java
@@ -0,0 +1,53 @@
+package com.daniela.creditflow.domain.valueObject;
+
+import com.daniela.creditflow.domain.exceptions.DomainException;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+public record Money(BigDecimal value) {
+
+    public Money {
+        Objects.requireNonNull(value, "Money cannot be null");
+
+        if (value.compareTo(BigDecimal.ZERO) < 0) {
+            throw new DomainException("Money cannot be negative");
+        }
+    }
+
+    public static Money zero() {
+        return new Money(BigDecimal.ZERO);
+    }
+
+    public boolean isZero() {
+        return value.compareTo(BigDecimal.ZERO) == 0;
+    }
+    public Money add(Money other) {
+        return new Money(value.add(other.value()));
+    }
+
+    public Money subtract(Money other) {
+        return new Money(value.subtract(other.value()));
+    }
+
+    public Money multiply(BigDecimal factor) {
+        return new Money(value.multiply(factor));
+    }
+
+    public boolean greaterThan(Money other) {
+        return value.compareTo(other.value()) > 0;
+    }
+    public boolean lessThan(Money other) {
+        return value.compareTo(other.value()) < 0;
+    }
+
+    public Money divide(Integer divisor) {
+
+        return new Money(
+                value.divide(
+                        BigDecimal.valueOf(divisor),
+                        2,
+                        RoundingMode.HALF_UP));
+    }
+}
