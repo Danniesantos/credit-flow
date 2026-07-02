e919181f991da6b217817581cebfcf2be4fa80e5
Danniesantos <danielardesantos@gmail.com>
2026-07-02 14:48:18 -0300

    feat(credit): implement credit calculation result

---
+++ src/main/java/com/daniela/creditflow/application/credit/calculation/CreditCalculationResult.java
@@ -0,0 +1,9 @@
+package com.daniela.creditflow.application.credit.calculation;
+
+import com.daniela.creditflow.domain.valueObject.InterestRate;
+import com.daniela.creditflow.domain.valueObject.Money;
+
+public record CreditCalculationResult(Money interestAmount,
+                                      Money totalAmount,
+                                      InterestRate interestRate) {
+}
