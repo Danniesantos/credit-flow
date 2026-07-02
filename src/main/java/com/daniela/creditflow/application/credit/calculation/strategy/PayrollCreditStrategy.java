9a9ee3e40b04a4911183623f69c97a3525c8331c
Danniesantos <danielardesantos@gmail.com>
2026-07-02 15:01:00 -0300

    feat(credit): implement payroll credit strategy

---
+++ src/main/java/com/daniela/creditflow/application/credit/calculation/strategy/PayrollCreditStrategy.java
@@ -0,0 +1,18 @@
+package com.daniela.creditflow.application.credit.calculation.strategy;
+
+import com.daniela.creditflow.domain.valueObject.InterestRate;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class PayrollCreditStrategy extends BaseCreditStrategy {
+
+    private static final InterestRate RATE =
+            new InterestRate(
+                    BigDecimal.valueOf(0.015));
+    @Override
+    protected InterestRate rate() {
+        return RATE;
+    }
+}
