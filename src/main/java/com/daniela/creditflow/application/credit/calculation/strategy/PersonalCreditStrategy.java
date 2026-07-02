399a6986d3ebab9d891c38b6b4ac0982f3c83281
Danniesantos <danielardesantos@gmail.com>
2026-07-02 15:03:13 -0300

    feat(credit): implement personal credit strategy

---
+++ src/main/java/com/daniela/creditflow/application/credit/calculation/strategy/PersonalCreditStrategy.java
@@ -0,0 +1,20 @@
+package com.daniela.creditflow.application.credit.calculation.strategy;
+
+import com.daniela.creditflow.domain.valueObject.InterestRate;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class PersonalCreditStrategy extends BaseCreditStrategy {
+
+    private static final InterestRate RATE =
+            new InterestRate(
+                    BigDecimal.valueOf(0.05));
+
+    @Override
+    protected InterestRate rate() {
+        return RATE;
+    }
+}
+
