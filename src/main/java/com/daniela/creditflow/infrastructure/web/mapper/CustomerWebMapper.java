58e904044448d89d0d4693a13aa42289825291e9
Danniesantos <danielardesantos@gmail.com>
2026-07-02 10:54:23 -0300

    feat(customer): implement customer web mapper

---
+++ src/main/java/com/daniela/creditflow/infrastructure/web/mapper/CustomerWebMapper.java
@@ -0,0 +1,57 @@
+package com.daniela.creditflow.infrastructure.web.mapper;
+
+import com.daniela.creditflow.application.customer.dto.input.CreateCustomerInput;
+import com.daniela.creditflow.application.customer.dto.input.UpdateCustomerInput;
+import com.daniela.creditflow.application.customer.dto.output.CustomerOutput;
+import com.daniela.creditflow.infrastructure.web.request.CustomerRequest;
+import com.daniela.creditflow.infrastructure.web.response.CustomerResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class CustomerWebMapper {
+
+    public CreateCustomerInput toInput(CustomerRequest request) {
+        return new CreateCustomerInput(
+                request.getName(),
+                request.getCpf(),
+                request.getEmail(),
+                request.getDateOfBirth(),
+                request.getPhoneNumber(),
+                request.getMonthlyIncome(),
+                request.getCreditScore()
+        );
+    }
+
+    public CustomerResponse toResponse(CustomerOutput output) {
+        return new CustomerResponse(
+                output.id(),
+                output.name(),
+                output.cpf(),
+                output.email(),
+                output.dateOfBirth(),
+                output.phoneNumber(),
+                output.monthlyIncome(),
+                output.creditScore(),
+                output.status(),
+                output.createdAt(),
+                output.updatedAt()
+        );
+    }
+
+    public UpdateCustomerInput updateToInput(UUID id,
+                                             CustomerRequest request) {
+
+        return new UpdateCustomerInput(
+                id,
+                request.getName(),
+                request.getCpf(),
+                request.getEmail(),
+                request.getDateOfBirth(),
+                request.getPhoneNumber(),
+                request.getMonthlyIncome(),
+                request.getCreditScore()
+        );
+    }
+}
