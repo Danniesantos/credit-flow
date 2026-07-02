876c47d2eae2bb258a51e1b6e72a532891df9d2b
Danniesantos <danielardesantos@gmail.com>
2026-07-02 14:31:07 -0300

    feat(credit): implement credit analysis handler

---
+++ src/main/java/com/daniela/creditflow/application/credit/analysis/CreditAnalysisHandler.java
@@ -0,0 +1,43 @@
+package com.daniela.creditflow.application.credit.analysis;
+
+import com.daniela.creditflow.application.credit.dto.output.AnalysisResult;
+import com.daniela.creditflow.domain.credit.model.Credit;
+import com.daniela.creditflow.domain.customer.model.Customer;
+
+public abstract class CreditAnalysisHandler {
+
+    private CreditAnalysisHandler next;
+
+    public CreditAnalysisHandler next(
+            CreditAnalysisHandler next) {
+
+        this.next = next;
+        return next;
+    }
+
+    public AnalysisResult handle(
+            Credit credit,
+            Customer customer) {
+
+        AnalysisResult result =
+                validate(
+                        credit,
+                        customer);
+
+        if (!result.approved()) {
+            return result;
+        }
+
+        if (next == null) {
+            return AnalysisResult.success();
+        }
+
+        return next.handle(
+                credit,
+                customer);
+    }
+
+    protected abstract AnalysisResult validate(
+            Credit credit,
+            Customer customer);
+}
