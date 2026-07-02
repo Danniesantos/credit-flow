6160502dbd8e3ef0151bb8bbbe831ed323ff126e
Danniesantos <danielardesantos@gmail.com>
2026-07-02 12:06:59 -0300

    feat(credit): implement analysis result

---
+++ src/main/java/com/daniela/creditflow/application/credit/dto/output/AnalysisResult.java
@@ -0,0 +1,31 @@
+package com.daniela.creditflow.application.credit.dto.output;
+
+public record AnalysisResult(boolean approved,
+                             String reason) {
+
+
+    public static AnalysisResult success() {
+        return new AnalysisResult(
+                true,
+                null);
+    }
+
+    public static AnalysisResult failure(String reason) {
+        return new AnalysisResult(
+                false,
+                reason);
+    }
+
+    public AnalysisResult {
+
+        if (approved && reason != null) {
+            throw new IllegalArgumentException(
+                    "Approved analysis cannot contain a reason");
+        }
+
+        if (!approved && reason.isBlank()) {
+            throw new IllegalArgumentException(
+                    "Rejected analysis must contain a reason");
+        }
+    }
+}
