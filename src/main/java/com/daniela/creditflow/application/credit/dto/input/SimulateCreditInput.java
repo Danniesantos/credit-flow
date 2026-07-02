540f30fae3adbf6cc4a558b5743d2dcf2fca9a61
Danniesantos <danielardesantos@gmail.com>
2026-07-02 11:58:08 -0300

    feat(credit): implement simulate credit input

---
+++ src/main/java/com/daniela/creditflow/application/credit/dto/input/SimulateCreditInput.java
@@ -0,0 +1,10 @@
+package com.daniela.creditflow.application.credit.dto.input;
+
+import com.daniela.creditflow.domain.credit.model.CreditType;
+
+import java.math.BigDecimal;
+
+public record SimulateCreditInput(BigDecimal requestedAmount,
+                                  Integer installments,
+                                  CreditType creditType) {
+}
