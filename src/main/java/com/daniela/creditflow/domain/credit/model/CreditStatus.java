326cde0d331de7cc64cb1ca2194a7a385af30a9a
Danniesantos <danielardesantos@gmail.com>
2026-07-03 07:38:43 -0300

    refactor(credit): centralize open credit statuses

--- src/main/java/com/daniela/creditflow/domain/credit/model/CreditStatus.java
+++ src/main/java/com/daniela/creditflow/domain/credit/model/CreditStatus.java
@@ -1,9 +1,20 @@
 package com.daniela.creditflow.domain.credit.model;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum CreditStatus {
     UNDER_ANALYSIS,
     APPROVED,
     REJECTED,
     CONTRACTED,
-    PAID_OFF
+    PAID_OFF;
+
+    public static Set<CreditStatus> openStatuses() {
+        return EnumSet.of(
+                UNDER_ANALYSIS,
+                APPROVED,
+                CONTRACTED
+        );
+    }
 }
