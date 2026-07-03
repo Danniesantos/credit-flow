a36b912bdef60763bad2a2dbe1ace7100c3567d4
Danniesantos <danielardesantos@gmail.com>
2026-07-03 06:56:02 -0300

    feat(credit): implement credit domain exceptions

--- src/main/java/com/daniela/creditflow/domain/exceptions/CreditNotFoundException.java
+++ src/main/java/com/daniela/creditflow/domain/exceptions/CreditNotFoundException.java
@@ -1,10 +1,10 @@
 package com.daniela.creditflow.domain.exceptions;
 
-import com.daniela.creditflow.domain.valueObject.CreditId;
+import com.daniela.creditflow.domain.credit.valueObject.CreditId;
 
 public class CreditNotFoundException extends RuntimeException {
     public CreditNotFoundException(CreditId creditId) {
 
-        super("Credit with id %s not found".formatted(creditId));
+        super("Credit with id %s not found".formatted(creditId.value()));
     }
 }
