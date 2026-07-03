f830243977fdbc2394b8dc185cdd02f70c4eeb4e
Danniesantos <danielardesantos@gmail.com>
2026-07-03 07:07:43 -0300

    feat(credit): implement credit domain exceptions

--- src/main/java/com/daniela/creditflow/domain/exceptions/InstallmentNotFoundException.java
+++ src/main/java/com/daniela/creditflow/domain/exceptions/InstallmentNotFoundException.java
@@ -1,7 +1,10 @@
 package com.daniela.creditflow.domain.exceptions;
 
+import com.daniela.creditflow.domain.valueObject.InstallmentId;
+
 public class InstallmentNotFoundException extends RuntimeException {
-    public InstallmentNotFoundException(String message) {
-        super(message);
+    public InstallmentNotFoundException(InstallmentId installmentId) {
+        super("Installment with id %s not found"
+                .formatted(installmentId.value()));
     }
 }
