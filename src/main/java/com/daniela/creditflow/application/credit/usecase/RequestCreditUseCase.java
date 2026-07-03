e92d172558a44b6622bd40594155ebfec412143f
Danniesantos <danielardesantos@gmail.com>
2026-07-03 06:36:15 -0300

    feat(credit): implement request credit use case

---
+++ src/main/java/com/daniela/creditflow/application/credit/usecase/RequestCreditUseCase.java
@@ -0,0 +1,38 @@
+package com.daniela.creditflow.application.credit.usecase;
+
+import com.daniela.creditflow.application.credit.dto.input.RequestCreditInput;
+import com.daniela.creditflow.application.credit.dto.output.RequestCreditOutput;
+import com.daniela.creditflow.application.credit.factory.CreditFactory;
+import com.daniela.creditflow.application.credit.mapper.CreditOutputMapper;
+import com.daniela.creditflow.domain.credit.model.Credit;
+import com.daniela.creditflow.domain.credit.repository.CreditRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RequestCreditUseCase {
+
+    private final CreditFactory creditFactory;
+    private final CreditRepository creditRepository;
+    private final CreditOutputMapper creditOutputMapper;
+
+    public RequestCreditUseCase(CreditFactory creditFactory,
+                                CreditRepository creditRepository,
+                                CreditOutputMapper creditOutputMapper) {
+
+        this.creditFactory = creditFactory;
+        this.creditRepository = creditRepository;
+        this.creditOutputMapper = creditOutputMapper;
+    }
+
+    public RequestCreditOutput execute(RequestCreditInput input) {
+
+        Credit credit =
+                creditFactory.create(input);
+
+        Credit saved =
+                creditRepository.save(credit);
+
+        return creditOutputMapper.toCreditOutput(saved);
+    }
+
+}
