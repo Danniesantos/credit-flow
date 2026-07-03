9d45b69f267e6cde08156476fe4726cf90df7b4c
Danniesantos <danielardesantos@gmail.com>
2026-07-03 14:58:37 -0300

    feat(infrastructure): add mapper for Installment persistence

---
+++ src/main/java/com/daniela/creditflow/infrastructure/persistence/installment/mapper/InstallmentMapper.java
@@ -0,0 +1,38 @@
+package com.daniela.creditflow.infrastructure.persistence.installment.mapper;
+
+import com.daniela.creditflow.domain.credit.valueObject.CreditId;
+import com.daniela.creditflow.domain.installment.model.Installment;
+import com.daniela.creditflow.domain.valueObject.InstallmentId;
+import com.daniela.creditflow.domain.valueObject.Money;
+import com.daniela.creditflow.infrastructure.persistence.credity.entity.CreditEntity;
+import com.daniela.creditflow.infrastructure.persistence.installment.entity.InstallmentEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InstallmentMapper {
+
+    public InstallmentEntity toEntity(Installment installment,
+                                      CreditEntity credit) {
+
+        return new InstallmentEntity(
+                installment.getId().value(),
+                installment.getNumber(),
+                installment.getAmount().value(),
+                installment.getDueDate(),
+                installment.getStatus(),
+                credit
+        );
+
+    }
+
+    public Installment toDomain(InstallmentEntity entity) {
+        return new Installment(
+                new InstallmentId(entity.getId()),
+                entity.getNumber(),
+                new Money(entity.getAmount()),
+                entity.getDueDate(),
+                entity.getStatus(),
+                new CreditId(entity.getCredit().getId())
+        );
+    }
+}
