309cb0c64a51253ee46fe499ea4d2bde3d53b1ab
Danniesantos <danielardesantos@gmail.com>
2026-07-24 11:44:18 -0300

    refactor(application): rename credit output mapper to application mapper

--- src/main/java/com/daniela/creditflow/application/credit/usecase/FindCreditUseCase.java
+++ src/main/java/com/daniela/creditflow/application/credit/usecase/FindCreditUseCase.java
@@ -1,24 +1,19 @@
 package com.daniela.creditflow.application.credit.usecase;
 
 import com.daniela.creditflow.application.credit.dto.output.CreditDetailsOutput;
-import com.daniela.creditflow.application.credit.mapper.CreditOutputMapper;
+import com.daniela.creditflow.application.credit.mapper.CreditApplicationMapper;
 import com.daniela.creditflow.application.credit.service.CreditService;
 import com.daniela.creditflow.domain.model.Credit;
 import com.daniela.creditflow.domain.valueObject.CreditId;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class FindCreditUseCase {
 
     private final CreditService creditService;
-    private final CreditOutputMapper creditMapper;
-
-    public FindCreditUseCase(CreditService creditService,
-                             CreditOutputMapper creditMapper) {
-
-        this.creditService = creditService;
-        this.creditMapper = creditMapper;
-    }
+    private final CreditApplicationMapper creditMapper;
 
     public CreditDetailsOutput execute(CreditId creditId) {
 
--- src/main/java/com/daniela/creditflow/application/credit/usecase/SimulateCreditUseCase.java
+++ src/main/java/com/daniela/creditflow/application/credit/usecase/SimulateCreditUseCase.java
@@ -1,23 +1,20 @@
 package com.daniela.creditflow.application.credit.usecase;
 
 import com.daniela.creditflow.application.credit.calculation.CreditCalculationResult;
-import com.daniela.creditflow.application.credit.service.CreditCalculationService;
 import com.daniela.creditflow.application.credit.dto.input.SimulateCreditInput;
 import com.daniela.creditflow.application.credit.dto.output.SimulateCreditOutput;
+import com.daniela.creditflow.application.credit.service.CreditCalculationService;
 import com.daniela.creditflow.domain.valueObject.Money;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SimulateCreditUseCase {
 
     private final CreditCalculationService calculationService;
 
-    public SimulateCreditUseCase(CreditCalculationService calculationService) {
-        this.calculationService = calculationService;
-    }
-
-    public SimulateCreditOutput execute(
-            SimulateCreditInput input) {
+    public SimulateCreditOutput execute(SimulateCreditInput input) {
 
         Money requestedAmount =
                 new Money(
