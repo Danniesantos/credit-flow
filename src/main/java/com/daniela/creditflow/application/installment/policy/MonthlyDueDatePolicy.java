5965daa4e0e919a997013619bd9176cc89fce404
Danniesantos <danielardesantos@gmail.com>
2026-07-30 18:25:14 -0300

    refactor: rename installment number to sequence

--- src/main/java/com/daniela/creditflow/application/installment/policy/MonthlyDueDatePolicy.java
+++ src/main/java/com/daniela/creditflow/application/installment/policy/MonthlyDueDatePolicy.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public class MonthlyDueDatePolicy implements DueDatePolicy {
 
     @Override
-    public LocalDate calculate(int installmentNumber, LocalDate referenceDate) {
-        return referenceDate.plusMonths(installmentNumber);
+    public LocalDate calculate(int sequence, LocalDate referenceDate) {
+        return referenceDate.plusMonths(sequence);
     }
 }
