c30d21738bf15c78d760dcbb6cc300c43dc6eb1d
Danniesantos <danielardesantos@gmail.com>
2026-08-07 13:03:41 -0300

    refactor(test): improve test constants

--- src/test/java/com/daniela/creditflow/support/TestConstants.java
+++ src/test/java/com/daniela/creditflow/support/TestConstants.java
@@ -4,6 +4,8 @@ import com.daniela.creditflow.domain.valueObject.InterestRate;
 import com.daniela.creditflow.domain.valueObject.Money;
 
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
 
 public class TestConstants {
 
@@ -18,4 +20,15 @@ public class TestConstants {
 
     public static final InterestRate FIVE_PERCENT =
             new InterestRate(BigDecimal.valueOf(0.05));
+
+    public static final Money INSTALLMENT_AMOUNT =
+            new Money(BigDecimal.valueOf(1_000));
+
+    public static final Integer INSTALLMENT_NUMBER = 1;
+
+    public static final LocalDate TEST_DATE =
+            LocalDate.of(2026, 8, 7);
+
+    public static final Instant PAID_AT =
+            Instant.parse("2026-08-07T12:00:00Z");
 }
