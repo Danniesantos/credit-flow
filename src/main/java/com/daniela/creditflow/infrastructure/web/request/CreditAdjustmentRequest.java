bfb6e0536a271e1ee91d20aa90054b4de4a35f0a
Danniesantos <danielardesantos@gmail.com>
2026-08-13 18:21:56 -0300

    feat(credit): add setter to adjustment request

--- src/main/java/com/daniela/creditflow/infrastructure/web/request/CreditAdjustmentRequest.java
+++ src/main/java/com/daniela/creditflow/infrastructure/web/request/CreditAdjustmentRequest.java
@@ -2,8 +2,10 @@ package com.daniela.creditflow.infrastructure.web.request;
 
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class CreditAdjustmentRequest {
 
     @Min(2)
