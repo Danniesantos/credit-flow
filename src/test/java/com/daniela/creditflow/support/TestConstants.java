90bede8359149d784dd6e10f92c71b1679171b5e
Danniesantos <danielardesantos@gmail.com>
2026-08-14 19:37:58 -0300

    refactor: rename test constants

--- src/test/java/com/daniela/creditflow/support/TestConstants.java
+++ src/test/java/com/daniela/creditflow/support/TestConstants.java
@@ -12,17 +12,14 @@ public class TestConstants {
     private TestConstants() {
     }
 
-    public static final Money TEN_THOUSAND =
+    public static final Money TOTAL_CREDIT_AMOUNT =
             new Money(BigDecimal.valueOf(10_000));
 
-    public static final Money ONE_THOUSAND =
+    public static final Money INSTALLMENT_AMOUNT =
             new Money(BigDecimal.valueOf(1_000));
 
     public static final InterestRate FIVE_PERCENT =
             new InterestRate(BigDecimal.valueOf(0.05));
-
-    public static final Money INSTALLMENT_AMOUNT =
-            new Money(BigDecimal.valueOf(1_000));
 
     public static final Integer INSTALLMENT_NUMBER = 1;
 
@@ -31,4 +28,16 @@ public class TestConstants {
 
     public static final Instant PAID_AT =
             Instant.parse("2026-08-07T12:00:00Z");
+
+    public static final String CUSTOMER_NAME = "Testando";
+
+    public static final LocalDate CUSTOMER_BIRTH_DATE =
+            LocalDate.of(1992, 1, 10);
+
+    public static final Money CUSTOMER_MONTHLY_INCOME =
+            new Money(BigDecimal.valueOf(5000));
+
+    public static final Instant CREATED_AT =
+            Instant.parse("2026-08-07T12:00:00Z");
+
 }
